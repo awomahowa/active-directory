8fdf383e848c1674fb9d2cd8cbca7eed8e8b8e1e
awohamowa <aloharespecia1013@gmail.com>
2025-12-14 14:53:22 +0800

    Made some changes to sagol file

--- subtest2.py
+++ subtest2.py
@@ -5,6 +5,7 @@ print("Student's name: " + studyante1.name)
 print("Student's major: " + studyante1.major)
 print("Student's gpa: " + str(studyante1.gpa))
 if studyante1.gpa >= 3.5:
-    print(studyante1.name + " is on honor roll")
+    print(studyante1.name + " "
+                            "is on honor roll")
 else:
     print("Student not on honor roll")
