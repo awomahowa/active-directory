a7d52e16a7185faeac385ee642cca49684375ede
awohamowa <aloharespecia1013@gmail.com>
2025-12-14 14:37:52 +0800

    Updated sagol.py

--- subtest2.py
+++ subtest2.py
@@ -1,6 +1,10 @@
 from subtest import Estudyante
 
-studyante1 = Estudyante("Calcifer", "Arts", 2.5, True)
+studyante1 = Estudyante("Calcifer", "Philosophy", 3.8, True)
 print("Student's name: " + studyante1.name)
 print("Student's major: " + studyante1.major)
 print("Student's gpa: " + str(studyante1.gpa))
+if studyante1.gpa >= 3.5:
+    print(studyante1.name + " is on honor roll")
+else:
+    print("Student not on honor roll")
